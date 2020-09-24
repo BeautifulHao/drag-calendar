564e7443adea6c69b4c2de10047e5f44072309ed
BeautifulHao <78714011@qq.com>
2020-09-24 15:32:54 +0800

    78714011@qq.com

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,11 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = async ({ config, mode }) => {
   config.module.rules.push({
     test: /\.scss$/,
-    include: path.resolve(__dirname, '../src'),
-    use: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.resolve(__dirname, "../src"),
+    use: ["style-loader", "css-loader", "sass-loader"],
   });
+
   return config;
 };
